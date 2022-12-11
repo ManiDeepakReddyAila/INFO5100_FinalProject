54e35b592f86b6b1ca6b3bc2fa40508f88ac52db
Mani Deepak Reddy Aila <manideepakreddyaila@Manis-MacBook-Air.local>
2022-12-10 21:42:47 -0500

    account controller added

---
+++ INFO5100_FinalProject/src/Controller/AccountController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import Classes.HealthcampRequests;
+import Classes.Accounts;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+
+/**
+ *
+ * @author manideepakreddyaila
+ */
+public class AccountController {
+    
+    public ArrayList<Accounts> getHealthcampRequests(){
+        ArrayList<Accounts> accountTable = new ArrayList<>();
+        try {
+            Statement statement = ConnectioinManager.getConnection().createStatement();
+            String query = "with temp as (SELECT a.*, b.test_name, b.lab_name FROM fp.healthcamp_requests a inner join fp.lab_requests b on a.patient_name = b.patient_name) select a.*, b.total_bill from temp a left join bill b on a.patient_name = b.patient_name";
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                accountTable.add(new Accounts(rs.getString("patient_name"), rs.getString("lab_name"), rs.getString("test_name"), rs.getString("hospital_name"), rs.getString("healthcamp_name"), rs.getString("doctor_assigned"), rs.getInt("total_bill")));
+            }
+            rs.close();
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return accountTable; 
+    }
+    
+}
